12d7fca8b2b1a1df1c21fd2f47fbc802e9b53775
Mike Procario <procario@gmail.com>
2015-11-16 21:52:07 -0500

    Simplify code
    Simplify code

--- day_counting.js
+++ day_counting.js
@@ -1,14 +1,14 @@
 
 function is_a_leap_year(year) {
 
-   if (year%4 === 0) {
-		if (year%400 === 0) {
-			return true;
-		} else if (year%100 === 0) {
-			return false;
-		}
+	if (year%400 === 0) {
 		return true;
-	} else {
-			return false;
-	}
+	} else if (year%100 === 0) {
+		return false;
+	} else if (year%4 === 0) {
+      return true;
+   } else {
+      return false;
+   }
+
 }
